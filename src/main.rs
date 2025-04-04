44b4aa5cc1e688200422fb91b4e028273351cf3d
Adam <adamdoxaev5@gmail.com>
2025-04-04 19:59:39 +0300

    project setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::{clone, net::TcpListener};
+
+use axum::Router;
+use serde::Serialize;
+use sqlx::{FromRow, PgPool};
+
+
+
+#[derive(Serialize, FromRow)]
+struct Item {
+    id: i32,
+    name: String,
+    description: String,
+}
+
+
+#[derive(Serialize)]
+struct RequestItem {
+    name: String,
+    description: String,
+}
+
+
+#[derive(Clone)]
+struct AppState {
+    dp_pool: PgPool,
+}
+
+impl AppState {
+
+}
+
+
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let db_pool = PgPool::connect(&database_url)
+        .await
+        .expect("Failed to connect to Postgres");
+
+
+    let app_state = AppState {
+        dp_pool: db_pool,
+    };
+
+    let app = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+
 }
